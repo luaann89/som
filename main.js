041c31818798208ce0c23dcd6374d3766fc45b20
luaann89 <127844960+luaann89@users.noreply.github.com>
2023-10-27 10:36:58 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-//crianda a função tocaSomAplauso e buscando id da tecla para tocar .
-function tocaSom(idElementoAudio).play();
+//crianda a função tocaSomAplauso e buscando id da tecla para tocar,
+function tocaSom( idElementoAudio)
+document . querySelector (idElementoAudio).play()
+1/referencia constante listaDeTeclas que busca todas as classes tecla
+const listaDeTeclas = document .querySelectorAll(".tecla")
+//criar a variavel de contagem
+let contador = 0
+//1aco de repetição while
+while (contador < listaDeTeclas.length)(
+faz funcionar som com teclas
+const efeito
+listaDeTeclas [contador].classlist[1]
+const idAudio = "#som_"+ efeito
+listaDeTeclas [contador ].onclick = function()(
+tocasom(idAudio)
+contador = contador
+console.log(contador)
